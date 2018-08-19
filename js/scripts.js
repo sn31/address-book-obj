7f05a794637d6dcf44d9f8fea204b0c4e21d9a9b
SkyeN <skye@dames.es>
2018-08-19 11:25:43 -0700

    add front end logic to obtain the first and last name inputs, create a new instance of Contact and display that when the form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
+// BACK-END LOGIC 
 function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
 }
+
+// FRONT-END LOGIC
+
+$(document).ready(function () {
+    $("form#new-contact").submit(function (event) {
+        event.preventDefault();
+        var firstNameInput = $("input#new-first-name").val();
+        var lastNameInput = $("input#new-last-name").val();
+        var newContact = new Contact(firstNameInput, lastNameInput);
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>")
+        $("input#new-first-name").val("");
+        $("input#new-last-name").val("");
+    });
+});
+
