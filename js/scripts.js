403ee1fe2e971df026699442f88dad8196f2af36
SkyeN <skye@dames.es>
2018-08-19 11:54:41 -0700

    add backend logic to show contact details

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,13 @@ $(document).ready(function () {
         $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>")
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
+        $(".contact").last().click(function () {
+            $("#show-contact").show();
+            $("#show-contact h2").text(newContact.firstName);
+            $(".firs-name").text(newContact.firstName);
+            $(".last-name").text(newContact.lastName);
+        });
     });
+
 });
 
