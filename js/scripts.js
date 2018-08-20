461fe9ca912c8437cae1394b21271745855308d6
SkyeN <skye@dames.es>
2018-08-20 13:39:57 -0700

    add new address fields

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,37 @@
 function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
-    this.addresses =[];
+    this.addresses = [];
 }
 
 function Address(street, city, state) {
-    this.street= street;
-    this.city= city;
+    this.street = street;
+    this.city = city;
     this.state = state;
 }
 
-Contact.prototype.fullName = function() {
-    return this.firstName + " " + this.lastName}
+Contact.prototype.fullName = function () {
+    return this.firstName + " " + this.lastName
+}
 // FRONT-END LOGIC
 
 $(document).ready(function () {
+    $("#add-address").click(function () {
+        $("#new-addresses").append('<div class="new-address">' +
+            '<div class="form-group">' +
+            '<label for="new-street">Street</label>' +
+            '<input type="text" class="form-control new-street">' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label for="new-city">City</label>' +
+            '<input type="text" class="form-control new-city">' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label for="new-state">State</label>' +
+            '<input type="text" class="form-control new-state">' +
+            '</div>' +
+            '</div>');
+    });
     $("form#new-contact").submit(function (event) {
         event.preventDefault();
         var firstNameInput = $("input#new-first-name").val();
