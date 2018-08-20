7028cc82c5cc1f575811d899dc302ffac22ce927
SkyeN <skye@dames.es>
2018-08-20 14:08:58 -0700

    add fullAddress prototype

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,9 @@ function Address(street, city, state) {
     this.state = state;
 }
 
+Address.prototype.fullAddress = function () {
+    return this.street + ", " + this.city + ", " + this.state;
+}
 Contact.prototype.fullName = function () {
     return this.firstName + " " + this.lastName
 }
@@ -55,7 +58,7 @@ $(document).ready(function () {
             $(".last-name").text(newContact.lastName);
             $("ul#addresses").text("");
             newContact.addresses.forEach(function (address) {
-                $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>")
+                $("ul#addresses").append("<li>" + address.fullAddress() + "</li>")
             })
             $("input#new-first-name").val("");
             $("input#new-last-name").val("");
